1797566d3912303848e4761ad98a18605ae63bf2
ccmikechen <ccmikechen@gmail.com>
2017-05-17 22:33:02 +0800

    add screens

--- src/app.js
+++ src/app.js
@@ -9,7 +9,27 @@ registerScreens();
 Navigation.startTabBasedApp({
   tabs: [{
       screen: 'petsgo.HomeScreen',
-      title: 'petsgo',
+      title: '首頁',
+      icon: require('../img/one.png'),
+      navigatorStyle: {},
+    }, {
+      screen: 'petsgo.SearchScreen',
+      title: '探索週邊',
+      icon: require('../img/one.png'),
+      navigatorStyle: {},
+    }, {
+      screen: 'petsgo.CameraScreen',
+      title: '拍照',
+      icon: require('../img/one.png'),
+      navigatorStyle: {},
+    }, {
+      screen: 'petsgo.AdoptScreen',
+      title: '寵物認養',
+      icon: require('../img/one.png'),
+      navigatorStyle: {},
+    }, {
+      screen: 'petsgo.InfoScreen',
+      title: '個人資訊',
       icon: require('../img/one.png'),
       navigatorStyle: {},
     }
---
+++ src/screens/AdoptScreen/AdoptScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from '../../containers/Home';
+
+const AdoptScreen = ({ navigator }) => (
+  <Home navigator={navigator} />
+);
+
+export default AdoptScreen;
