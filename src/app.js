be4e11cb854b3078cc55c03af15636cdd87c4ccd
JohnnyYu0930 <a3660980@gmail.com>
2017-05-17 22:28:03 +0800

    add image swiper and tabs view and add toolbar icon

--- src/app.js
+++ src/app.js
@@ -7,10 +7,35 @@ import { registerScreens } from './screens';
 registerScreens();
 
 Navigation.startTabBasedApp({
-  tabs: [{
+  tabs: [
+    {
       screen: 'petsgo.HomeScreen',
       title: 'petsgo',
-      icon: require('../img/one.png'),
+      icon: require('../img/首頁.png'),
+      navigatorStyle: {},
+    },
+    {
+      screen: 'petsgo.HomeScreen',
+      title: 'petsgo',
+      icon: require('../img/地圖.png'),
+      navigatorStyle: {},
+    },
+    {
+      screen: 'petsgo.HomeScreen',
+      title: 'petsgo',
+      icon: require('../img/相機.png'),
+      navigatorStyle: {},
+    },
+    {
+      screen: 'petsgo.HomeScreen',
+      title: 'petsgo',
+      icon: require('../img/領養.png'),
+      navigatorStyle: {},
+    },
+    {
+      screen: 'petsgo.HomeScreen',
+      title: 'petsgo',
+      icon: require('../img/個人資訊.png'),
       navigatorStyle: {},
     }
   ],
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
+var ScrollableTabView = require('react-native-scrollable-tab-view');
 
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -8,13 +12,63 @@ class Home extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>
-          Hello, World!
-        </Text>
-      </View>
+      <View style={{flex:1}}>
+        <View style={{flex:1}}>
+          <Swiper style={styles.wrapper}
+            height="100%"
+            showsButtons={true}>
+            <View style={styles.slide1}>
+              <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+            <View style={styles.slide2}>
+              <Text style={styles.text}>Beautiful</Text>
+            </View>
+            <View style={styles.slide3}>
+              <Text style={styles.text}>And simple</Text>
+            </View>
+          </Swiper>
+        </View>
+        <ScrollableTabView
+          style={{flex:2}}>
+          <FirstRoute tabLabel="全部" />
+          <SecondRoute tabLabel="協尋"/>
+          <SecondRoute tabLabel="活動"/>
+          <SecondRoute tabLabel="送養"/>
+          <SecondRoute tabLabel="看護"/>
+        </ScrollableTabView>
+          </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex:1
+  },
+  wrapper: {
 
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+});
 export default Home;
