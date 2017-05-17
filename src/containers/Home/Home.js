0344e77817116913bcd9f05c56d3b8680abf6fbf
JohnnyYu0930 <a3660980@gmail.com>
2017-05-17 22:34:26 +0800

    add home screen

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,8 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import Swiper from 'react-native-swiper';
 var ScrollableTabView = require('react-native-scrollable-tab-view');
 
-const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
-const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -14,61 +13,31 @@ class Home extends React.Component {
     return (
       <View style={{flex:1}}>
         <View style={{flex:1}}>
-          <Swiper style={styles.wrapper}
+          <Swiper
             height="100%"
             showsButtons={true}>
-            <View style={styles.slide1}>
-              <Text style={styles.text}>Hello Swiper</Text>
+            <View>
+              
             </View>
-            <View style={styles.slide2}>
-              <Text style={styles.text}>Beautiful</Text>
+            <View>
+
             </View>
-            <View style={styles.slide3}>
-              <Text style={styles.text}>And simple</Text>
+            <View>
+
             </View>
           </Swiper>
         </View>
         <ScrollableTabView
           style={{flex:2}}>
-          <FirstRoute tabLabel="全部" />
-          <SecondRoute tabLabel="協尋"/>
-          <SecondRoute tabLabel="活動"/>
-          <SecondRoute tabLabel="送養"/>
-          <SecondRoute tabLabel="看護"/>
+          <View tabLabel="全部" />
+          <View tabLabel="協尋"/>
+          <View tabLabel="活動"/>
+          <View tabLabel="送養"/>
+          <View tabLabel="看護"/>
         </ScrollableTabView>
-          </View>
+      </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex:1
-  },
-  wrapper: {
 
-  },
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
-});
 export default Home;
