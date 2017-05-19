ea872ad6bf4cd84cfbbcf802f2f49754b74e9a8a
ccmikechen <ccmikechen@gmail.com>
2017-05-19 23:31:22 +0800

    add Adopt

---
+++ src/containers/Adopt/Adopt.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { View } from 'react-native';
+import styles from './styles';
+
+class Adopt extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+export default Adopt;
