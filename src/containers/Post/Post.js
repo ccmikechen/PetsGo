41b9a63d019ed2dbd884a95c3e90b121ec3713bb
JohnnyYu0930 <a3660980@gmail.com>
2017-06-03 03:20:20 +0800

    change post style to styles

--- src/containers/Post/Post.js
+++ src/containers/Post/Post.js
@@ -27,23 +27,18 @@ class Post extends React.Component {
   render() {
     let user = this.props.post.user;
     return (
-      <ScrollView style={{flex:1, backgroundColor: '#f5f1f1',padding:15}}>
-        <View style={{flexDirection: 'row'}}>
+      <ScrollView style={styles.container}>
+        <View style={styles.header}>
           <Image
-            style={{
-              height: 80,
-              width: 80,
-              borderRadius: 40,
-              marginTop: 10,
-            }}
+            style={styles.userimage}
             source={{uri: 'https://d36lyudx79hk0a.cloudfront.net/p0/mn/p2/c16531eda44144f6.jpg'}}
           />
-          <Text style={{flex:1,marginTop:40,marginLeft:10,fontSize:16}}>{`${user.first_name}${user.last_name}`}</Text>
+          <Text style={styles.username}>{`${user.first_name}${user.last_name}`}</Text>
         </View>
 
-        <Text style={{fontSize:25,color:'#000000',marginTop:10,fontWeight:'bold'}}>{this.props.post.title}</Text>
-        <Text style={{color:'#626161',marginTop:10}}>{`${this.parseTypeToName(this.props.post.type)}版 ${new Date(this.props.post.updated_at).getFullYear()}/${new Date(this.props.post.updated_at).getMonth()+1}/${new Date(this.props.post.updated_at).getDate()} ${new Date(this.props.post.updated_at).getHours()}:${new Date(this.props.post.updated_at).getMinutes()} `}</Text>
-        <Text style={{fontSize:18,marginTop:20,color:'#626161',marginBottom:25}}>{this.props.post.content}</Text>
+        <Text style={styles.title}>{this.props.post.title}</Text>
+        <Text style={styles.typedatetime}>{`${this.parseTypeToName(this.props.post.type)}版 ${new Date(this.props.post.updated_at).getFullYear()}/${new Date(this.props.post.updated_at).getMonth()+1}/${new Date(this.props.post.updated_at).getDate()} ${new Date(this.props.post.updated_at).getHours()}:${new Date(this.props.post.updated_at).getMinutes()} `}</Text>
+        <Text style={styles.content}>{this.props.post.content}</Text>
       </ScrollView>
     );
   }
